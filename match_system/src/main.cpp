d896d04a35b989a3a4d231fb9ceaa49a51ac7a38
yyy1004 <3218253@qq.com>
2025-08-04 11:20:37 +0800

    match server 3.0 for match after sort

--- match_system/src/main.cpp
+++ match_system/src/main.cpp
@@ -42,11 +42,25 @@ class Pool {
 
         void match() {
             while (users.size() > 1) {
-                auto a = users[0], b = users[1];
-                users.erase(users.begin());
-                users.erase(users.begin());
+                // auto a = users[0], b = users[1];
+                // users.erase(users.begin());
+                // users.erase(users.begin());
+                sort(users.begin(), users.end(), [&](User &a, User &b) {
+                        return a.score < b.score;
+                        });
 
-                save_result(a.id, b.id);
+                bool flag = true;
+                for (uint32_t i = 1; i < users.size(); i ++ ) {
+                    auto a = users[i - 1], b = users[i];
+                    if (b.score - a.score <= 50) {
+                        users.erase(users.begin() + i - 1, users.begin() + i + 1);
+                        save_result(a.id, b.id);
+
+                        flag = false;
+                        break;
+                    }
+                }
+                if (flag) break;;
             }
         }
 
@@ -109,7 +123,10 @@ void consume_task() {
     while (true) {
         unique_lock<mutex> lck(message_queue.m);
         if (message_queue.q.empty()) {
-            message_queue.cv.wait(lck);
+            //message_queue.cv.wait(lck);
+            lck.unlock();
+            pool.match();
+            sleep(1);
         } else {
             auto task = message_queue.q.front();
             message_queue.q.pop();
